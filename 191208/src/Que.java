e622450bb0ddd8214ba081bd1c921dc90701b537
juljasong <juljasong@gmail.com>
2019-12-09 00:00:42 +0900

    1208

---
+++ 191208/src/Que.java
@@ -0,0 +1,15 @@
+
+public class Que {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		int i;
+		
+		for ( i = 1 ; i <= 10 ; i = i + 3 ) {
+			sum += i;
+		}
+		System.out.printf("%d %d", i, sum);
+
+	}
+
+}
---
+++ 191208/src/While888.java
@@ -0,0 +1,16 @@
+
+public class While888 {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		int sum = 0;
+		while( i <= 888) {
+			sum += i;
+			i++;
+		}
+		System.out.printf("1부터 888 까지의 합은 %d 입니다.", sum);
+
+	}
+
+}
