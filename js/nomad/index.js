bccd6256a5d3cb5c6007580eb668f432d9fb2343
juljasong <juljasong@gmail.com>
2020-01-21 00:25:27 +0900

    0121

--- js/nomad/index.js
+++ js/nomad/index.js
@@ -1,22 +1,22 @@
 /* DOM : Document Object Model */
 
 const title = document.querySelector('#title');
-title.innerHTML = '집에 가고 싶어요';
+//title.innerHTML = '집에 가고 싶어요';
+const CLICKED_CLASS = 'clicked';
 
-function handleClickRed() {
-  title.style.color = 'red';
+function handleClick() {
+  /*
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if(hasClass){
+    title.classList.remove(CLICKED_CLASS);
+  } else {
+    title.classList.add(CLICKED_CLASS);
+  }*/
+  title.classList.toggle(CLICKED_CLASS);
 }
 
-function handleClickBlue() {
-  title.style.color = '#34495e';
+function init() {
+  title.addEventListener('click', handleClick);
 }
 
-//title.addEventListener('click', handleClick);
-//console.log(title);
-
-
-if(title.style.color === 'red') {
-  title.addEventListener('click', handleClickBlue);
-} else {
-  title.addEventListener('click', handleClickRed);
-}
+init();
